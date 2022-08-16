73cb8ef545a9430700991f1190ed77771cbf4e39
Mint Missy <dawidkostka480@gmail.com>
2022-08-17 00:26:35 +0200

    Generate modules; Active router link fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
     loadChildren: () => import('./home/home.module').then((module) => module.HomeModule)
-  }
+  },
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) },
+  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) }
 ];
 
 @NgModule({
